ee6b3ade2683f2ec38cdf3c17e4d959967dccf92
dromero <dante.romero@tecso.coop>
2022-10-07 19:36:49 -0300

    Strategy design pattern

---
+++ java-design-patterns-examples/src/strategy/example/strategies/PayStrategy.java
@@ -0,0 +1,12 @@
+package strategy.example.strategies;
+
+/**
+ * Common interface for all strategies.
+ */
+public interface PayStrategy {
+
+	boolean pay(int paymentAmount);
+    
+	void collectPaymentDetails();
+    
+}
